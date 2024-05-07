bd8a95341026fe9cad419f74bad5a5cfa5087900
Hailey Stoudt <stoudth@oregonstate.edu>
2024-05-07 14:18:45 -0700

    Added shell code to both tests.py and check_pwd.py. Currently no functioning unittests and check_pwd only returns True as recommended in assignment's Canvas Page.

---
+++ TDDAssignment/check_pwd.py
@@ -0,0 +1,7 @@
+# Name: Hailey Stoudt
+# Course: CS 362
+# Assignment : TDD Testing
+
+
+def check_pwd(string):
+    return True
---
+++ TDDAssignment/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from check_pwd import check_pwd
+
+
+class TestCheckPassword(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
